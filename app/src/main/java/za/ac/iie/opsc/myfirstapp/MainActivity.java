9a4783fe0a3eaa29bcd1bad5d9e1c801c7c382c7
mashudu <mluvhengo25@gmail.com>
2022-04-27 15:35:47 +0200

    Ice task 2

---
+++ app/src/main/java/za/ac/iie/opsc/myfirstapp/MainActivity.java
@@ -0,0 +1,31 @@
+package za.ac.iie.opsc.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextClock;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickGermany(View view) {
+        TextClock clock = findViewById(R.id.my_clock);
+        clock.setTimeZone("Europe/Berlin");
+    }
+
+    public void onClickIndia(View view) {
+        TextClock clock = findViewById(R.id.my_clock);
+        clock.setTimeZone("Asia/Kolkata");
+    }
+
+    public void onClickSouthAfrica(View view) {
+        TextClock clock = findViewById(R.id.my_clock);
+        clock.setTimeZone("Africa/CapeTown");
+    }
+}
